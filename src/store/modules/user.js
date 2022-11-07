2e22efe1e1d1a04ba9a9d15a1362288418be5b51
Artbat <116061172+raider-82@users.noreply.github.com>
2022-11-08 00:44:07 +0500

    Login Module Updated

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,99 +1,117 @@
-import axios from 'axios';
+import axios from "axios";
 import jwt_decode from "jwt-decode";
-import { isAuthGuardActive, apiUrl } from '../../constants/config'
-import { setCurrentUser, getCurrentUser } from '../../utils'
+import { isAuthGuardActive, apiUrl } from "../../constants/config";
+import { setCurrentUser, getCurrentUser } from "../../utils";
 
 export default {
   state: {
     // currentUser: isAuthGuardActive ? getCurrentUser() : currentUser,
-    currentUser: null,
+    currentUser: {
+      id: Number,
+      first_name: String,
+      last_name: String,
+      user_name: String,
+      email: String,
+      role: String,
+      status: String,
+      image_url: String,
+      address: String,
+    },
     loginError: null,
     processing: false,
     forgotMailSuccess: null,
-    resetPasswordSuccess: null
+    resetPasswordSuccess: null,
   },
   getters: {
-    currentUser: state => state.currentUser,
-    processing: state => state.processing,
-    loginError: state => state.loginError,
-    forgotMailSuccess: state => state.forgotMailSuccess,
-    resetPasswordSuccess: state => state.resetPasswordSuccess,
+    currentUser: (state) => state.currentUser,
+    processing: (state) => state.processing,
+    loginError: (state) => state.loginError,
+    forgotMailSuccess: (state) => state.forgotMailSuccess,
+    resetPasswordSuccess: (state) => state.resetPasswordSuccess,
   },
   mutations: {
     setUser(state, payload) {
-      state.currentUser = payload
-      state.processing = false
-      state.loginError = null
+      state.currentUser = payload;
+      state.processing = false;
+      state.loginError = null;
     },
     setLogout(state) {
-      state.currentUser = null
-      state.processing = false
-      state.loginError = null
+      state.currentUser = null;
+      state.processing = false;
+      state.loginError = null;
     },
     setProcessing(state, payload) {
-      state.processing = payload
-      state.loginError = null
+      state.processing = payload;
+      state.loginError = null;
     },
     setError(state, payload) {
-      state.loginError = payload
-      state.currentUser = null
-      state.processing = false
+      state.loginError = payload;
+      state.currentUser = null;
+      state.processing = false;
     },
     setForgotMailSuccess(state) {
-      state.loginError = null
-      state.currentUser = null
-      state.processing = false
-      state.forgotMailSuccess = true
+      state.loginError = null;
+      state.currentUser = null;
+      state.processing = false;
+      state.forgotMailSuccess = true;
     },
     setResetPasswordSuccess(state) {
-      state.loginError = null
-      state.currentUser = null
-      state.processing = false
-      state.resetPasswordSuccess = true
+      state.loginError = null;
+      state.currentUser = null;
+      state.processing = false;
+      state.resetPasswordSuccess = true;
     },
     clearError(state) {
-      state.loginError = null
-    }
+      state.loginError = null;
+    },
   },
   actions: {
     async login({ commit }, payload) {
+      var res = await axios.post(apiUrl + "auth/login", payload);
 
-      var res = await axios.post(apiUrl+"auth/login", payload);
-      console.log(res);
-
-      if (res.status==201) {
-        commit('clearError');
-        commit('setProcessing', true);
-        var data = jwt_decode(res.data.access_token)
-        console.log(data)
-        user => {
-          // setCurrentUser(item)
-          // commit('setUser', item)
-        };
+      if (res.status == 201) {
+        commit("clearError");
+        commit("setProcessing", true);
+        var data = jwt_decode(res.data.access_token);
+        console.log(data.id);
+        var item = { id: data.id, ...data };
+        // {
+        //   id: data.id,
+        //   first_name: data.firstNamem,
+        //   last_name: data.lastName,
+        //   user_name: data.userName,
+        //   email: data.userEmail,
+        //   role: data.roles,
+        //   status: data.status,
+        //   image_url: data.ImageUrl,
+        //   address: data.Address
+        // }
+        setCurrentUser(item);
+        commit("setUser", item);
         return true;
       } else {
         return false;
       }
-      
+
       // .signInWithEmailAndPassword(payload.email, payload.password)
       // .then(
-      
+
       // err => {
       //   setCurrentUser(null);
       //   commit('setError', err.message)
       // }
     },
     forgotPassword({ commit }, payload) {
-      commit('clearError')
-      commit('setProcessing', true)
+      commit("clearError");
+      commit("setProcessing", true);
       // firebase
       //   .auth()
       //   .sendPasswordResetEmail(payload.email)
       //   .then(
-      user => {
-        commit('clearError')
-        commit('setForgotMailSuccess')
-      }
+      (user) => {
+        commit("clearError");
+        commit("setForgotMailSuccess");
+      };
       // err => {
       //   commit('setError', err.message)
       //   setTimeout(() => {
@@ -103,16 +121,16 @@ export default {
       // )
     },
     resetPassword({ commit }, payload) {
-      commit('clearError')
-      commit('setProcessing', true)
+      commit("clearError");
+      commit("setProcessing", true);
       // firebase
       //   .auth()
       //   .confirmPasswordReset(payload.resetPasswordCode, payload.newPassword)
       //   .then(
-      user => {
-        commit('clearError')
-        commit('setResetPasswordSuccess')
-      }
+      (user) => {
+        commit("clearError");
+        commit("setResetPasswordSuccess");
+      };
       //   err => {
       //     commit('setError', err.message)
       //     setTimeout(() => {
@@ -122,10 +140,9 @@ export default {
       // )
     },
 
-
     signOut({ commit }) {
       setCurrentUser(null);
-      commit('setLogout');
-    }
-  }
-}
+      commit("setLogout");
+    },
+  },
+};
