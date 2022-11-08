af2f7e19599803cd301110f84d91f46af29dbaa3
Artbat <116061172+raider-82@users.noreply.github.com>
2022-11-09 03:20:37 +0500

    dist

---
+++ dist/assets/js/second-menu.5595eedd.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["second-menu"],{"19df":function(n,e,u){"use strict";u.r(e);var t=function(){var n=this,e=n._self._c;return e("router-view")},o=[],r=u("2877"),s={},c=Object(r["a"])(s,t,o,!1,null,null,null);e["default"]=c.exports}}]);
